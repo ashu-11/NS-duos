48f8ddc21012e8f1b4d371a62b79432bdd9b78da
Ashutosh Mishra <ashutosh.mishra@quantiphi.com>
2022-09-09 09:51:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+constructor(){
+  super();
+  this.state= {
+    counter:0
+  }
+
 }
+render() {
+    return <div>
+              <h1 className="counter">{this.state.counter}</h1>
+              <button  onClick={()=>{
+               this.setState({counter:this.state.counter+1})
+              }}>increment</button>
+            </div>
+  }
+  
+}
+
+
 
 export default App;
