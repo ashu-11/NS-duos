fe545d8862f103de6fbede50a13e64c23832367d
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2022-09-09 10:38:55 +0530

    Create React_lecture1.js

---
+++ React_lecture1.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class App extends React.Component {
+  
+// render is a mandatory method always use this method   
+render() {
+    return <div>
+            Hello World
+            </div>
+  }
+  
+}
+
+
+
+export default App;
