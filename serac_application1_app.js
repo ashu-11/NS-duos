0b1d86ae9a7929b0ed8fa7f84ebab130bb379817
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2022-09-19 22:34:16 +0530

    Update serac_application1_app.js
    Added search functionality also

--- serac_application1_app.js
+++ serac_application1_app.js
@@ -8,19 +8,36 @@ import  axios from 'axios'
 function App() {
 
   const[photosList,setPhotosList]=useState([]);
+  const[searchItem,setSearchItem]=useState("");
+  const[filterphotosList,setFilterPhotosList]=useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
 
   useEffect(()=>{
 
     axios.get('https://jsonplaceholder.typicode.com/photos').then((res)=>{
-      console.log("data from APi ",res.data);
+      setIsLoading(false);
       setPhotosList(res.data)
+      // setFilterPhotosList(res.data)
     })
 
   },[])
+
+  useEffect(()=>{
+      const filteredArray  = photosList.filter((photo)=>{
+        return photo.title.includes(searchItem)
+      })
+      setFilterPhotosList(filteredArray);
+  },[searchItem])
+
+  const searchItems=(value)=>{
+    // value whatever we have type in search box
+      setSearchItem(value)
+  }
   return (
     <div className="App">
-      <SearchBar></SearchBar>
-      <ListItems listOfItems={photosList}></ListItems>
+      <SearchBar searchItems={searchItems} searchValue={searchItem}  ></SearchBar>
+      <ListItems listOfItems={filterphotosList} isLoading={isLoading}></ListItems>
     </div>
   );
 }
