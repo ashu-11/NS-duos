f4cb7bafe8b4a4213fbfbccbfc48536e8690c15a
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2022-10-04 09:18:27 +0530

    Create Error_boundry_BuggyCounter.js

---
+++ Error_boundry_BuggyCounter.js
@@ -0,0 +1,23 @@
+import React from "react";
+class BuggyCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(({ counter }) => ({
+      counter: counter + 1
+    }));
+  }
+
+  render() {
+    if (this.state.counter === 5) {
+      // Simulate a JS error
+      throw new Error("I crashed!");
+    }
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
+  }
+}
+export default BuggyCounter;
